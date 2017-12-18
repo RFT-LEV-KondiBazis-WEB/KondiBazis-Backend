2d3d8572a3207de857a1ce043eb0364e75665a1f
Bak Balázs <bak.balazs95@gmail.com>
2017-12-18 20:10:37 +0100

    fix

--- fitbase-parent/fitbase-service-impl/src/main/java/hu/unideb/fitbase/service/api/rules/pass/duration/PassTimeDurationNotNull.java
+++ fitbase-parent/fitbase-service-impl/src/main/java/hu/unideb/fitbase/service/api/rules/pass/duration/PassTimeDurationNotNull.java
@@ -2,7 +2,6 @@ package hu.unideb.fitbase.service.api.rules.pass.duration;
 
 import hu.unideb.fitbase.commons.pojo.request.PassCreateRequest;
 import hu.unideb.fitbase.commons.pojo.validator.Violation;
-import hu.unideb.fitbase.service.api.domain.Pass;
 import hu.unideb.fitbase.service.api.validator.rule.Rule;
 import org.springframework.stereotype.Component;
 
@@ -10,20 +9,22 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static hu.unideb.fitbase.commons.constants.rules.pass.duration.PassDurationConstants.PASS_NOT_ADD_NULL;
 import static hu.unideb.fitbase.commons.constants.rules.pass.duration.PassTimeDurationConstants.BLANK_RULE;
 import static hu.unideb.fitbase.commons.constants.rules.pass.duration.PassTimeDurationConstants.FIELD;
 
 @Component
-public class PassTimeDurationNotBeBlankRule implements Rule<PassCreateRequest> {
+public class PassTimeDurationNotNull implements Rule<PassCreateRequest> {
 
     @Override
     public List<Violation> validate(PassCreateRequest request){
         List<Violation> result = Collections.<Violation>emptyList();
         String timeDuration = request.getTimeDuration();
-        if (timeDuration == null ) {
+        System.out.println(timeDuration);
+        if (timeDuration.equals("0") ) {
             result = Arrays.asList(Violation.builder()
                     .field(FIELD)
-                    .validationMessage(BLANK_RULE)
+                    .validationMessage(PASS_NOT_ADD_NULL)
                     .build());
         }
         return result;
